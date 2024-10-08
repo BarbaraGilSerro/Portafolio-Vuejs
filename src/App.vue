668a5fd4babe33887e26385df9ac9a55d4104419
Barbara gil <ingbabaragil@gmail.com>
2024-10-08 19:36:44 -0300

    Clase 3 Portfolio: Limpieza del proyecto, funcionamiento de Vue composition API

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,18 @@
 <script setup>
-
 //App.vue es un archivo que la raiz de nuestra aplicacion y puede cargar archivos en Js y 
 //ejecutar en forma de html y darle estilos
 //Aca se coloca todo java script
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+const nombreApellido='Babsy'
 </script>
 
 <template>
   <!---Aca empieza el html-->
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+<header>
+  <h1>{{nombreApellido}}</h1>
+  <p> Bienvenido a Vue 3</p>
+</header>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
